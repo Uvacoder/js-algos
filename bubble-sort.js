7e4e93ffc81dea92d1373561dba7d775626d8911
Christian Boyle <mail@christianboyle.com>
2021-03-07 07:10:09 -0600

    Add bubble sort

---
+++ bubble-sort.js
@@ -0,0 +1,20 @@
+let bubbleSort = (inputArr) => {
+    let len = inputArr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 0; i < len; i++) {
+            if (inputArr[i] > inputArr[i + 1]) {
+                let tmp = inputArr[i];
+                inputArr[i] = inputArr[i + 1];
+                inputArr[i + 1] = tmp;
+                swapped = true;
+            }
+        }
+    } while (swapped);
+    return inputArr;
+};
+
+const inputArr = [5,3,1,4,6];
+
+console.log(bubbleSort(inputArr)); // [1,3,4,5,6]
